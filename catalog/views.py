66c7b79f5cee1383b4b751803ca9e6f6cd05673d
dmitriy.balashov <reaver_std@mail.ru>
2023-08-30 15:26:28 +0500

    homework 18_2 step 5

--- catalog/views.py
+++ catalog/views.py
@@ -6,4 +6,9 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'You have new message from {name}({phone}): {message}')
     return render(request, 'catalog/contacts.html')

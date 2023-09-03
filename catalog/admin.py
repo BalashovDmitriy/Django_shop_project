5095922b5fd04d70954ac38e11531f69dd931b09
dmitriy.balashov <reaver_std@mail.ru>
2023-09-03 14:07:17 +0500

    homework 20_1 step 2

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from catalog.models import Product, Category
+
+
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    text_fields = ('name', 'description', 'price', 'image',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    text_fields = ('name',)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+
+    class Meta:
+        verbose_name = 'Наименование'
+        verbose_name_plural = 'Наименования'
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'

d4c54bf26cdd914aeb6d6d859aa835ec9d6ecf3c
dmitriy.balashov <reaver_std@mail.ru>
2023-09-04 10:33:41 +0500

    homework 20_1 step star

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Product, Category
+from catalog.models import *
 
 
 # Register your models here.
@@ -14,3 +14,6 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name',)
+
+
+admin.site.register(Contacts)
--- catalog/models.py
+++ catalog/models.py
@@ -31,3 +31,16 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+
+
+class Contacts(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    phone = models.CharField(max_length=15, verbose_name='Телефон')
+    message = models.TextField(verbose_name='Сообщение')
+
+    def __str__(self):
+        return f"{self.name}({self.phone})"
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'

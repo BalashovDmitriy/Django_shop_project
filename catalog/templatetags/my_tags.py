843f9ad802caf559d03a44e089e1ee3dd978a8c0
dmitriy.balashov <reaver_std@mail.ru>
2023-09-06 11:58:17 +0500

    homework 20_2 step 4

---
+++ catalog/templatetags/my_tags.py
@@ -0,0 +1,17 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter()
+def mediapath(value):
+    if value:
+        return f'/media/{value}'
+    return ''
+
+
+@register.simple_tag()
+def mediapath(value):
+    if value:
+        return f'/media/{value}'
+    return ''

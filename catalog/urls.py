0a73f848be4adf775f7d8e688cff7f5183526704
dmitriy.balashov <reaver_std@mail.ru>
2023-09-07 12:41:14 +0500

    homework 20_2 step star-1

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,5 +5,7 @@ from catalog import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
-    path('product/<int:pk>', views.product, name='product')
+    path('product/<int:pk>', views.product, name='product'),
+    path('cat_create/', views.create_category, name='category_create'),
+    path('product_create/', views.create_product, name='product_create'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from catalog.models import *
 
 
@@ -19,3 +19,30 @@ def contacts(request):
 
 def product(request, pk):
     return render(request, 'catalog/product.html', {'product': Product.objects.get(pk=pk)})
+
+
+def create_category(request):
+    if request.method == 'POST':
+        cat_name = request.POST.get('cat_name')
+        cat_desc = request.POST.get('cat_desc')
+        Category.objects.create(name=cat_name, description=cat_desc)
+        return redirect('home')
+    return render(request, 'catalog/cat_create.html')
+
+
+def create_product(request):
+    if request.method == 'POST':
+        prod_category = request.POST.get('prod_category')
+        name = request.POST.get('prod_name')
+        price = request.POST.get('prod_price')
+        description = request.POST.get('prod_desc')
+        prod_image = request.FILES.get('prod_image')
+        Product.objects.create(name=name, description=description, price=price,
+                               image=prod_image, category=Category.objects.get(id=prod_category))
+        print(f"Данные:\n"
+              f"Название: {name}\n"
+              f"Описание: {description}\n"
+              f"Цена: {price}\n"
+              f"Фото: {prod_image}\n"
+              f"Категория: {prod_category}")
+    return render(request, 'catalog/prod_create.html', {'categories': Category.objects.all()})
